ad3a8fe9e61a363a496585eb1d373c724530a37c
hussein <hussein@souleiman.de>
2025-09-18 18:33:52 +0200

    added berliner

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function startTickerAnimation() {
 const sheetID = '10mfm9SVVDiWcxnfK2QuUCj3msaVFBQIQx34NnPlUEo4';
 const url = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
 
-const columns = ["Eingang", "Hadi","Ramazan", "Osama", "Geprüft"];
+const columns = ["Eingang","Hadi" ,"Ramazan", "Hussein", "Osama", "Geprüft"];
 
 let lastFetchTime = null;
 
@@ -118,6 +118,7 @@ function renderBoard(data) {
     Eingang: [],
     Hadi: [],
     Ramazan: [],
+    Hussein: [],
     Osama: [],
     Geprüft: []
   };
