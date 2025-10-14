fa39221f636cb7d9c6a5acfecfa8d6c7adc1b9bc
hussein <hussein@souleiman.de>
2025-10-14 15:53:22 +0200

    removed osama emoji

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const sheetID = '10mfm9SVVDiWcxnfK2QuUCj3msaVFBQIQx34NnPlUEo4';
 const url = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
 
 // Spalten-Definition und Board-Map automatisch synchron halten
-const columns = ["Eingang", "Hadi", "Ramazan", "Osama 🧛", "Geprüft"];
+const columns = ["Eingang", "Hadi", "Ramazan", "Osama", "Geprüft"];
 function makeEmptyMap() {
   return columns.reduce((m, c) => (m[c] = [], m), {});
 }
