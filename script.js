417cd20c32710813734193ef3a796751a49ec90b
hussein <hussein@souleiman.de>
2026-01-20 15:17:16 +0100

    changes emojis

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ async function fetchData() {
 
     const nextNummer = nummern.length ? Math.max(...nummern) + 1 : '–';
 
-    setTickerText(`🎆 Aktuelle Nummer: ${nextNummer} 🎉`);
+    setTickerText(`💥Aktuelle Nummer: ${nextNummer} 🚗`);
 
     renderBoard(cleaned);
     lastFetchTime = new Date();   // Zeitpunkt des echten Abrufs
